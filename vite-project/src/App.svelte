30aeb5096759bc3f373bc54417993b5c77cccb1c
karim-daman <38837234+karim-daman@users.noreply.github.com>
2024-07-21 16:08:24 +0100

    push

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -3,7 +3,20 @@
   import routes from "./routes";
   import Navbar from "$components/Navigation.svelte";
   import Footer from "$components/Footer.svelte";
+
+  import toast, { Toaster } from "svelte-french-toast";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    toast.success("It works!");
+
+    toast.success("Always at the bottom.", {
+      position: "bottom-center",
+    });
+  });
 </script>
+
+<Toaster />
 
 <Navbar />
 
